72809304b6fc2036d51bc9f8f1cf80c2cdb40baa
Dan Billson <danny.billson@hotmail.com>
2023-05-28 16:15:30 +0100

    🎨 center account

--- src/pages/account.tsx
+++ src/pages/account.tsx
@@ -3,10 +3,8 @@ import { type NextPage } from "next";
 
 const Account: NextPage = () => {
   return (
-    <main className="flex flex-col justify-center">
-      <div className=" center container flex flex-col items-center gap-12 px-4 py-16 ">
-        <UserProfile />
-      </div>
+    <main className="flex flex-col items-center gap-12 px-4 py-16">
+      <UserProfile />
     </main>
   );
 };
