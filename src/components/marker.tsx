6d45f743ddd03b93446e4629dc1bf2f4cd7ef0f4
Dan Billson <danny.billson@hotmail.com>
2023-05-31 21:50:34 +0100

    ✨ feat: add google map location to bar view

---
+++ src/components/marker.tsx
@@ -0,0 +1,48 @@
+import { type Bar } from "@prisma/client";
+import * as Avatar from "@radix-ui/react-avatar";
+import * as HoverCard from "@radix-ui/react-hover-card";
+import Link from "next/link";
+
+type MarkerProps = {
+  bar: Bar;
+};
+
+export const Marker = ({ bar }: MarkerProps) => {
+  const location = bar.location as { lat: number; lon: number };
+
+  return (
+    <HoverCard.Root
+      // @ts-expect-error - google-maps-react-markers adds lat and lng
+      lat={location.lat}
+      lng={location.lon}
+    >
+      <HoverCard.Trigger>
+        <Link href={`/bar/${bar.id}`}>
+          <Avatar.Root className="inline-flex h-[45px] w-[45px] select-none items-center justify-center overflow-hidden rounded-full bg-slate-900 align-middle">
+            <Avatar.Fallback className="leading-1 flex h-full w-full items-center justify-center bg-white text-[15px] font-medium text-orange11">
+              {bar.name
+                .split(" ")
+                .map((w) => w[0])
+                .slice(0, 2)}
+            </Avatar.Fallback>
+          </Avatar.Root>
+        </Link>
+      </HoverCard.Trigger>
+      <HoverCard.Portal>
+        <HoverCard.Content
+          className="w-64 rounded-md bg-white p-5 shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] data-[side=bottom]:animate-slideUpAndFade data-[side=left]:animate-slideRightAndFade data-[side=right]:animate-slideLeftAndFade data-[side=top]:animate-slideDownAndFade data-[state=open]:transition-all"
+          sideOffset={5}
+        >
+          <div className="flex flex-col">
+            <p className="mb-4">{bar.name}</p>
+            <p className="text-xs text-slate-600">{bar.line1}</p>
+            {bar.line2 && <p className="text-xs text-slate-600">{bar.line2}</p>}
+            <p className="text-xs text-slate-600">{bar.city}</p>
+            <p className="text-xs text-slate-600">{bar.postcode}</p>
+          </div>
+          <HoverCard.Arrow className="fill-white" />
+        </HoverCard.Content>
+      </HoverCard.Portal>
+    </HoverCard.Root>
+  );
+};
--- src/pages/bar/[id].tsx
+++ src/pages/bar/[id].tsx
@@ -1,6 +1,7 @@
+import Map from "google-maps-react-markers";
 import type { GetStaticProps, NextPage } from "next";
 import { Layout } from "~/components/layout";
-
+import { Marker } from "~/components/marker";
 import { generateSSGHelper } from "~/server/helpers/ssgHelper";
 import { api } from "~/utils/api";
 
@@ -11,9 +12,21 @@ const Bar: NextPage<{ id: string }> = ({ id }) => {
 
   const { bar } = data;
 
+  const location = bar.location as { lat: number; lon: number };
+
   return (
     <Layout>
       <h1 className="text-2xl font-bold">{bar.name}</h1>
+
+      <div className="h-96 w-full py-4">
+        <Map
+          apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}
+          defaultCenter={{ lat: location.lat, lng: location.lon }}
+          defaultZoom={17}
+        >
+          <Marker bar={bar} />
+        </Map>
+      </div>
     </Layout>
   );
 };
