81b4a4de100c03fee73f88add36ff5508d2ff7ec
Dan Billson <danny.billson@hotmail.com>
2023-06-18 20:42:25 +0100

    ✨ feat: add beverage filters
    Ideally this should be done in the db query
    with https://www.prisma.io/docs/concepts/components/prisma-client/full-text-search#adding-indexes however,
    this didn't seem to work with relations so
    client side filters and sorts were the fallback

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -6,6 +6,7 @@ import { SearchForm } from "~/components/search-form";
 import { api } from "~/utils/api";
 import { getDistance } from "geolib";
 import Link from "next/link";
+import type { Bar, BarBeverage, Beverage, Brewery } from "@prisma/client";
 
 type SearchQuery = {
   location: string;
@@ -30,6 +31,9 @@ const Search: NextPage = () => {
     }
   );
 
+  const bars =
+    data?.bars.filter(hasBrewery(brewery)).filter(hasBeerStyle(style)) || [];
+
   return (
     <main className="flex flex-col items-center justify-center">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
@@ -42,10 +46,10 @@ const Search: NextPage = () => {
         {data && data.bars.length === 0 && (
           <p className="pb-4">No bars found, try searching again</p>
         )}
-        {data && data.bars.length > 0 ? (
+        {data && bars.length > 0 ? (
           <>
             <div className="grid w-full grid-cols-1 gap-4 px-6 md:grid-cols-2">
-              {data.bars.map((bar) => (
+              {bars.map((bar) => (
                 <div
                   key={bar.id}
                   className="flex flex-col border border-black p-4 text-slate-600"
@@ -74,15 +78,18 @@ const Search: NextPage = () => {
                       <p className="mt-2 text-sm font-bold text-slate-800">
                         Preview
                       </p>
-                      {bar.beverages.slice(0, 4).map(({ beverage }) => (
-                        <div key={beverage.id}>
-                          <span className="text-xs text-slate-600">
-                            {beverage.brewery.name}{" "}
-                            <span className="font-bold">{beverage.name}</span> -{" "}
-                            {beverage.style}
-                          </span>
-                        </div>
-                      ))}
+                      {bar.beverages
+                        .sort(byStyleAndBrewery(style, brewery))
+                        .slice(0, 4)
+                        .map(({ beverage }) => (
+                          <div key={beverage.id}>
+                            <span className="text-xs text-slate-600">
+                              {beverage.brewery.name}{" "}
+                              <span className="font-bold">{beverage.name}</span>{" "}
+                              - {beverage.style}
+                            </span>
+                          </div>
+                        ))}
                     </div>
                   )}
 
@@ -104,7 +111,7 @@ const Search: NextPage = () => {
                 }}
                 defaultZoom={13}
               >
-                {data.bars.map((bar) => (
+                {bars.map((bar) => (
                   <Marker
                     key={bar.id}
                     bar={bar}
@@ -120,5 +127,51 @@ const Search: NextPage = () => {
     </main>
   );
 };
+
+type BarWithBeverages = Bar & {
+  beverages: (BarBeverage & { beverage: Beverage & { brewery: Brewery } })[];
+};
+
+const hasBeerStyle = (style?: string) => (bar: BarWithBeverages) => {
+  if (!style) return true;
+  return bar.beverages.some((beverage) =>
+    beverage.beverage.style.includes(style)
+  );
+};
+
+const hasBrewery = (brewery?: string) => (bar: BarWithBeverages) => {
+  if (!brewery) return true;
+  return bar.beverages.some((beverage) =>
+    beverage.beverage.brewery.name.includes(brewery)
+  );
+};
+
+type BeverageWithBrewery = BarBeverage & {
+  beverage: Beverage & { brewery: Brewery };
+};
+
+const byStyleAndBrewery =
+  (style?: string, brewery?: string) =>
+  (a: BeverageWithBrewery, b: BeverageWithBrewery) => {
+    if (!style && !brewery) return 0;
+    if (style && !brewery) {
+      if (a.beverage.style.includes(style)) return -1;
+      if (b.beverage.style.includes(style)) return 1;
+      return 0;
+    }
+    if (!style && brewery) {
+      if (a.beverage.brewery.name.includes(brewery)) return -1;
+      if (b.beverage.brewery.name.includes(brewery)) return 1;
+      return 0;
+    }
+    if (style && brewery) {
+      if (a.beverage.style.includes(style)) return -1;
+      if (b.beverage.style.includes(style)) return 1;
+      if (a.beverage.brewery.name.includes(brewery)) return -1;
+      if (b.beverage.brewery.name.includes(brewery)) return 1;
+      return 0;
+    }
+    return 0;
+  };
 
 export default Search;
