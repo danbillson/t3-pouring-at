07bae200ff4372a1f29768d2932fbae08f9f48b4
Dan Billson <danny.billson@hotmail.com>
2023-06-21 17:40:53 +0100

    ♻️ refactor: update beverage card to use card

---
+++ src/components/beverage-card.tsx
@@ -0,0 +1,75 @@
+import type { BarBeverage, Beverage, Brewery } from "@prisma/client";
+import { api } from "~/utils/api";
+import { Button } from "~/components/ui/button";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "~/components/ui/card";
+
+dayjs.extend(relativeTime);
+
+type BeverageCardProps = {
+  barBeverage: BarBeverage & { beverage: Beverage & { brewery: Brewery } };
+  edit?: boolean;
+  deleting?: boolean;
+};
+
+export const BeverageCard = ({ barBeverage, edit }: BeverageCardProps) => {
+  const { barId, beverageId, beverage, tappedOn } = barBeverage;
+  const ctx = api.useContext();
+  const { mutate, isLoading, variables } = api.barBeverage.untap.useMutation({
+    onSuccess: () => {
+      void ctx.bars.getBySlug.invalidate();
+    },
+  });
+
+  return (
+    <Card
+      className={`${
+        isLoading && variables?.beverageId === beverageId
+          ? "border-red-500"
+          : "border-black"
+      }`}
+    >
+      <CardHeader>
+        <CardTitle>
+          <span className="font-normal">{beverage.brewery.name}</span>{" "}
+          {beverage.name}
+        </CardTitle>
+        <CardDescription>
+          <span className="mr-2 text-sm">
+            {String(beverage.abv)}%<span className="mx-1 text-xs">ABV</span>
+          </span>
+          <span className="text-sm text-slate-900">{beverage.style}</span>
+        </CardDescription>
+      </CardHeader>
+
+      <CardContent>
+        <span className="text-sm">
+          Tapped: <span className="font-bold">{dayjs(tappedOn).fromNow()}</span>
+        </span>
+      </CardContent>
+
+      {edit && (
+        <CardFooter>
+          <Button
+            className="ml-auto underline"
+            variant="ghost"
+            disabled={isLoading}
+            onClick={() => {
+              mutate({ barId, beverageId });
+            }}
+          >
+            Untap
+          </Button>
+        </CardFooter>
+      )}
+    </Card>
+  );
+};
--- src/components/beverage-list.tsx
+++ src/components/beverage-list.tsx
@@ -1,10 +1,5 @@
 import type { BarBeverage, Beverage, Brewery } from "@prisma/client";
-import { api } from "~/utils/api";
-import { Button } from "~/components/ui/button";
-import dayjs from "dayjs";
-import relativeTime from "dayjs/plugin/relativeTime";
-
-dayjs.extend(relativeTime);
+import { BeverageCard } from "~/components/beverage-card";
 
 type BeverageListProps = {
   beverages: (BarBeverage & { beverage: Beverage & { brewery: Brewery } })[];
@@ -12,57 +7,15 @@ type BeverageListProps = {
 };
 
 export const BeverageList = ({ beverages, edit }: BeverageListProps) => {
-  const ctx = api.useContext();
-  const { mutate, isLoading, variables } = api.barBeverage.untap.useMutation({
-    onSuccess: () => {
-      void ctx.bars.getBySlug.invalidate();
-    },
-  });
-
   return (
     <div className="my-6 grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
-      {beverages.map(({ barId, beverageId, beverage, tappedOn }) => {
+      {beverages.map((barBeverage) => {
         return (
-          <div
-            key={beverage.id}
-            className={`border-2 border-solid ${
-              isLoading && variables?.beverageId === beverageId
-                ? "border-red-500"
-                : "border-black"
-            } flex flex-col justify-between px-4 py-2`}
-          >
-            <div>
-              <div>
-                <p className="mr-2 text-lg">
-                  {beverage.brewery.name}{" "}
-                  <span className="font-bold">{beverage.name}</span>
-                </p>
-              </div>
-              <div>
-                <span className="mr-2 text-sm">
-                  {String(beverage.abv)}%
-                  <span className="mx-1 text-xs text-slate-500">ABV</span>
-                </span>
-                <span className="text-sm">{beverage.style}</span>
-              </div>
-              <span className="mt-3 block text-sm">
-                Tapped:{" "}
-                <span className="font-bold">{dayjs(tappedOn).fromNow()}</span>
-              </span>
-            </div>
-            {edit && (
-              <Button
-                className="ml-auto mt-3 underline"
-                variant="ghost"
-                disabled={isLoading}
-                onClick={() => {
-                  mutate({ barId, beverageId });
-                }}
-              >
-                Untap
-              </Button>
-            )}
-          </div>
+          <BeverageCard
+            key={barBeverage.barId}
+            barBeverage={barBeverage}
+            edit={edit}
+          />
         );
       })}
     </div>
