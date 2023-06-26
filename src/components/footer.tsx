d2451aec80a46a82ba1d516bf63b080464b94da7
Dan Billson <danny.billson@hotmail.com>
2023-06-26 20:58:39 +0100

    ✨ feat: add ko-fi link

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,10 +1,7 @@
 export const Footer = () => {
   return (
-    <footer
-      className="w-full border-t text-slate-600
-    "
-    >
-      <div className="mx-auto w-full max-w-6xl p-8">
+    <footer className="w-full border-t text-slate-600">
+      <div className="mx-auto w-full max-w-6xl p-8 text-sm">
         Built by{" "}
         <a
           href="https://danbillson.com"
@@ -13,6 +10,15 @@ export const Footer = () => {
           className="underline"
         >
           danbillson
+        </a>
+        . Found some good beers?{" "}
+        <a
+          href="https://ko-fi.com/danbillson"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="underline"
+        >
+          Support me
         </a>
       </div>
     </footer>
