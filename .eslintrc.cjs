f3e79353be75b1e244a28f2ad19b27c6ea6fd5fa
Juan <4lzeck@gmail.com>
2024-02-09 20:29:16 +0000

    🩹 fix(eslint): fix broken linting file

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -30,8 +30,7 @@ const config = {
     ],
     "@typescript-eslint/no-unused-vars": [
       "warn",
-      { argsIgnorePattern: "^_" },
-      { varsIgnorePattern: "^_" },
+      { argsIgnorePattern: "^_" , varsIgnorePattern: "^_" },
     ],
   },
 };
